1c3e3f26661d2c4965750bb8d08c74c9a060a2d8
merc <merc@suigin.local>
2018-09-23 12:17:03 +0200

    - load from configuration file

--- unitykram.tests/CompositionTest.cs
+++ unitykram.tests/CompositionTest.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+using Microsoft.Practices.Unity.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Unity;
 using Unity.Injection;
@@ -18,6 +20,27 @@ namespace unitykram.tests
             Assert.IsNotNull(dependee);
             Assert.IsNotNull(dependee.Dependency);
 
+        }
+
+        [TestMethod]
+        public void ConfigurationComposition()
+        {
+            var mapping = new ExeConfigurationFileMap(){ExeConfigFilename = "unity.config"};
+            var configuration = ConfigurationManager.OpenMappedExeConfiguration(mapping, ConfigurationUserLevel.None);
+            var container = new UnityContainer();
+
+            if (!(configuration.GetSection("unity") is UnityConfigurationSection section))
+            {
+                Assert.Fail("section named unity is not of type UnityConfigurationSection");
+                return;
+            }
+            
+            container.LoadConfiguration(section);
+            
+            var dependee =  container.Resolve<IDependee>();
+            
+            Assert.IsNotNull(dependee);
+            Assert.IsNotNull(dependee.Dependency);
         }
     }
 }
